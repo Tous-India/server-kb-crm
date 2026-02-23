ecf677eab9dc20348753877864f1ab209ee394a2
Manawwar <manawwarsaifi1010@gmail.com>
2026-02-23 23:11:19 +0530

    mongodb error resolve

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
+import dns from "node:dns/promises";
 import config from "./index.js";
 
 const connectDB = async () => {
   try {
+    // Set DNS servers to resolve MongoDB Atlas SRV records
+    dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
     const conn = await mongoose.connect(config.mongoUri);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
